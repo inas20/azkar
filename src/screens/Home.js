f6d02447431cae01619144649d2cf7fadb0a710a
inas20 <inaass.07@gmail.com>
2020-12-02 11:15:35 +0200

    remove unwanted code

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import {Text, View, TouchableOpacity, StyleSheet, Dimensions, ScrollView} from 'react-native';
-import { v4 as uuidv4 } from 'uuid';
+import {Text, View, StyleSheet, Dimensions, ScrollView} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import { WerdCard } from '../components/werdComponent';
-import {ZekrCard}  from '../components/zekrComponent';
-import AZKAR from '../DataSet/azkar.json';
 import { HomeCard } from '../components/homeCard';
 
 const{height, width}= Dimensions.get('window')
@@ -13,36 +10,6 @@ const{height, width}= Dimensions.get('window')
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props)
-    this.state={
-      azkar: [],
-    }
-  }
-
-  componentDidMount(){
-    this.setupData()
-  }
-
-  setupData=()=>{
-    let azkar = AZKAR;
-    azkar.forEach(zekr=>{
-      zekr.id= uuidv4()
-    },this.setState({azkar: azkar}))
-  }
-
-
-  showZekr =(item)=>{
-    let newAzkar= this.state.azkar.slice();
-    let filtered = []
-    filtered= newAzkar.filter(zekr=>zekr.id !=item.id) 
-    this.setState({azkar: filtered})
-  }
-
-
-
-  renderZekrItem=({item})=>{
-    return(
-      <ZekrCard zekr={item}  showZekr={(item)=> this.showZekr(item)}/>
-    )
   }
 
   render() {
@@ -54,7 +21,6 @@ export default class HomeScreen extends React.Component {
               <Ionicons name="md-sunny-outline" color={"yellow"} size={87} style={{margin :3}}/>
               <Text style={[styles.welcomeText,{marginTop:10}]}>ربنا يتقبل منا </Text>
             </View>
-           
         </View>
         <View style={{flexDirection:'row',justifyContent:'space-between'}}>
           <WerdCard navigation={this.props.navigation} title={"المساء"}/>
